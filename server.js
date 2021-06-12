d5efe15b44d6ec113ef6cccec9759fc985e28587
royster <rcantu786@gmail.com>
2021-06-11 19:20:42 -0500

    finalized routes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Tardation420!',
+    password: '',
     database: 'election'
   },
   console.log('Connected to the election database.')
